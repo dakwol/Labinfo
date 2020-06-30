bd45f2b71793c35cb84dafffea561aa47f097eac
dakwol <53054385+dakwol@users.noreply.github.com>
2020-06-30 12:04:27 +0300

    Update 7lab19var.cpp

--- 7lab19var.cpp
+++ 7lab19var.cpp
@@ -3,65 +3,67 @@
 #include <stdlib.h>
 #include <locale.h>
 #define N 5
-int Me(int A[][N])
+int Me(int A[][N])//функция ввода матрицы с клавиатуры
+{
+   int i,j;
+   for (j=0;j<N;j++)
+      for (i=0;i<N;i++)
+        scanf("%d" , &A[i][j]);
+}
+
+int file(int A[][N])//функция чтения матрицы из файла
+{
+   int i, j; FILE* f;
+   f = fopen("in.txt", "r");//открытие файла для чтения
+   if (f != NULL)//проверка открытия файла
+      for (i = 0; i<N; i++)//считывание матрицы из файла
+         for (j = 0; j<N; j++)
+         fscanf(f, "%d", &A[i][j]);
+   else printf("Входной файл отсутствует");
+   fclose(f);//закрытие файла
+}
+int rec(int Y,int X[], int stepi)//определяем Y
+{
+    if (stepi< 4)
+      if (X[stepi] % 2 != 0)
+        {
+          Y*= X[stepi] ; rec(Y,X, stepi + 1);
+        }
+      else return Y;
+    else return Y;
+}
+int output(int A[][N], int X[])//функция вывода матрицы на экран и в файл(вместе с X и Y)
+{
+   FILE* f; int Y =0;
+   for (int i = 0; i<N; i++)//вывод матрицы на экран
     {
-        int i,j;
-        for (j=0;j<N;j++)
-            for (i=0;i<N;i++)
-                scanf("%d" , &A[i][j]);
+        for (int j = 0; j< N; j++)
+        printf("%3d", A[i][j]);
+        printf("\n");
     }
-int file(int A[][N])
+   for (int i = 0; i< N; i++)//вывод X и Y на экран
+   printf("\nx[%d]=%d", i+1, X[i]);
+   printf("\nY = %d", rec(1,X, 0));
+   f = fopen("out.txt", "w");//открытие файла для записи
+   if (f != NULL)//Проверка открытия файла
     {
-        int i, j; FILE* f;
-        f = fopen("in", "r");
-        if (f != NULL)
-            for (j = 0; j<N; j++)
-                for (i = 0; i<N; i++)
-                    fscanf(f, "%d", &A[i][j]);
-        else printf("Входной файл отсутствует");
-        fclose(f);
+       for (int i = 0; i< N; i++)//запись в файл матрицы
+        {
+          for (int j = 0; j< N; j++)
+              fprintf(f, "%3d", A[i][j]);
+              fprintf(f, "\n");
+       }
+       for (int i = 0; i < N; i++)//запись в файл X и Y
+           fprintf(f, "\nx[%d]=%d", i+1, X[i]);
+           fprintf(f, "\nY = %d", rec(1,X, 0));
     }
-int rec(int X[], int stepi)
-    {
-        if (stepi< 4)
-            if (X[stepi] % 2 != 0)
-                return X[stepi] * rec(X, stepi + 1);
-            else return 1;
-        else return 1;
-    }
-int output(int A[][N], int X[])
-    {
-        FILE* f; int Y =0;
-        for (int j = 0; j<N; j++)
-            {
-                for (int i = 0; i< N; i++)
-                printf("%3d", A[i][j]);
-                printf("\n");
-            }
-            for (int i = 0; i< N; i++)
-            printf("\nx[%d]=%d", i+1, X[i]);
-            printf("\nY = %d", rec(X, 0));
-            f = fopen("out", "w");
-            if (f != NULL)
-                {
-                    for (int i = 0; i< N; i++)
-                        {
-                            for (int j = 0; j< N; j++)
-                                fprintf(f, "%3d", A[j][i]);
-                            fprintf(f, "\n");
-                        }
-                    for (int i = 0; i < N; i++)
-                        fprintf(f, "\nx[%d]=%d", i+1, X[i]);
-                    fprintf(f, "\nY = %d", rec(X, 0));
-                }
-            else printf("Ошибка открытия файла");
-    }
+    else printf("Ошибка открытия файла");
+}
 int *MainCalculation(int A[][5],int (*yka)(int[][N] ),int X[])
-    {
+{
         int min,Check=false;
         int j,i,t; (*yka)(A);
-        // Поиск минимального элемента
-	    min=A[0][0];
+	    min=A[0][0];// Поиск минимального элемента в матрице
         for(i=0;i<N;i++)
             {
                 for(j=0;j<N;j++)
@@ -72,17 +74,18 @@ int *MainCalculation(int A[][5],int (*yka)(int[][N] ),int X[])
 			         }
 		        }
             }
-            printf("минимальный эл.%d\n",min);
-            for (i = 0; i < 5; i++)
+            printf("Минимальный элемент матрицы: %d\n",min);
+            for (i = 0; i < N; i++)//нахождение минимального элемента в строках
                {
-                    for (j = 0; j < 5; j++)
+                    for (j = 0; j < N; j++)
                        {
-                          if (A[i][j]==min)
+                          if (A[i][j]==min)//если найденный элемент в строке равен минимальному в матрице, то присваиваем значение истина
                           {
                             Check=true;
                           }
                        }
-                    if (Check)
+
+                    if (Check)//если значение истино, записываем 1, в противном случае -1
                     {
                       X[i]=1;
                     }
@@ -92,16 +95,17 @@ int *MainCalculation(int A[][5],int (*yka)(int[][N] ),int X[])
                     }
                   Check=false;
                }
+             return X;//возвращаем X
     }
 int main()
-    {
-        char* locale = setlocale(LC_ALL,"");
-        int A[N][N], X[N], k; int *Xpointer;
-        int (*yka)(int[][N]) = NULL;
-        printf("Укажите какой ввод матрицы\nC клавиатуры - введите 1\nC файла - 0\n");
-        printf("Ваш выбор - "); scanf("%d", &k);
-        if (k == 1) yka = Me;
-        else yka = file;
-        Xpointer = MainCalculation(A,yka, X);
-        output(A, Xpointer);
-    }
+{
+    char* locale = setlocale(LC_ALL,"");
+    int A[N][N], X[N], k; int *Sp;
+    int (*yka)(int[][N]) = NULL;
+    printf("Укажите какой ввод матрицы\nC клавиатуры - введите 1\nC файла - введите 0\n");
+    printf("Ваш выбор - "); scanf("%d", &k);
+    if (k == 1) yka = Me;
+    else yka = file;
+    Sp = MainCalculation(A,yka, X);
+    output(A, Sp);
+}
